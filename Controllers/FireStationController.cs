6e755680b7d9f0708ee0083230c193d26e34f4cf
MAX <max.waterschoot@student.hogent.be>
2023-12-29 13:10:54 +0100

    Folder, mapping

--- Controllers/FireStationController.cs
+++ Controllers/FireStationController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LSFD.Stations
+namespace LSFD.Controllers
 {
     internal class FireStationController
     {
--- Data/Data.cs
+++ Data/Data.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LSFD.MainClasses
+namespace LSFD.Data
 {
     internal class Data
     {
